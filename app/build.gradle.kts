04372312eca728394bef6029dca76fe7ba50c1fc
kurt bryant <BCArpilleda@gmail.com>
2024-11-27 13:45:39 +0800

    GOD loves all of us

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,20 +40,9 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
     implementation ("androidx.appcompat:appcompat:1.7.0")
     implementation ("androidx.compose.material:material:1.7.5")
     implementation ("androidx.lifecycle:lifecycle-service:2.8.7")
