bbf23df00c6a3718b00219fb03df54c4850d676a
kurt bryant <BCArpilleda@gmail.com>
2025-01-10 15:55:45 +0800

    GOD loves all of us

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.customkeyboard"
         minSdk = 28
         targetSdk = 34
-        versionCode = 13
-        versionName = "2.2"
+        versionCode = 14
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
