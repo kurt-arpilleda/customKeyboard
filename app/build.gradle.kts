7a91d187e29cc41c05e0a832fef4bbd7b570f8bb
Kurt Bryant <BCArpilleda@gmail.com>
2025-04-24 09:14:34 +0800

    hello

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.customkeyboard"
         minSdk = 28
         targetSdk = 34
-        versionCode = 33
-        versionName = "4.2"
+        versionCode = 34
+        versionName = "4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
